8a4b77c88ecdb6ea644f2cbc98083fe2a7fda1da
alu0101278919 <alu0101278919@ull.edu.es>
2022-06-04 12:58:07 +0100

    Modifications in graph

--- FCheddar/mainwindow.cpp
+++ FCheddar/mainwindow.cpp
@@ -7,43 +7,51 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     QLineSeries *series = new QLineSeries();
-    *series << QPointF(0, 6) << QPointF(9, 4) << QPointF(15, 20) << QPointF(25, 12) << QPointF(29, 26);
+    QLineSeries *seriesTask = new QLineSeries();
+    *series << QPointF(0, 0.75) << QPointF(120, 0.75);
+    *seriesTask << QPointF(0,0.75) << QPointF(5, 0.75);
     QChart *chart = new QChart();
     chart->legend()->hide();
     chart->addSeries(series);
+    chart->addSeries(seriesTask);
 
     // Customize series
     QPen pen(QRgb(0xfdb157));
     pen.setWidth(5);
     series->setPen(pen);
 
+    QPen penTask(QRgb(0xfdb157));
+    pen.setWidth(20);
+    seriesTask->setPen(penTask);
+
     // Customize chart title
     QFont font;
     font.setPixelSize(18);
     chart->setTitleFont(font);
-    chart->setTitleBrush(QBrush(Qt::white));
-    chart->setTitle("Customchart example");
+    chart->setTitleBrush(QBrush(Qt::black));
+    chart->setTitle("FCheddar");
 
     // Customize chart background
-    QLinearGradient backgroundGradient;
+    /*QLinearGradient backgroundGradient;
     backgroundGradient.setStart(QPointF(0, 0));
     backgroundGradient.setFinalStop(QPointF(0, 1));
     backgroundGradient.setColorAt(0.0, QRgb(0xd2d0d1));
     backgroundGradient.setColorAt(1.0, QRgb(0x4c4547));
     backgroundGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
     chart->setBackgroundBrush(backgroundGradient);
+    */
 
     // Customize plot area background
-    QLinearGradient plotAreaGradient;
+    /*QLinearGradient plotAreaGradient;
     plotAreaGradient.setStart(QPointF(0, 1));
     plotAreaGradient.setFinalStop(QPointF(1, 0));
     plotAreaGradient.setColorAt(0.0, QRgb(0x555555));
     plotAreaGradient.setColorAt(1.0, QRgb(0x55aa55));
     plotAreaGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
     chart->setPlotAreaBackgroundBrush(plotAreaGradient);
-    chart->setPlotAreaBackgroundVisible(true);
+    chart->setPlotAreaBackgroundVisible(true);*/
 
-    QCategoryAxis *axisX = new QCategoryAxis();
+    QValueAxis *axisX = new QValueAxis;
     QCategoryAxis *axisY = new QCategoryAxis();
 
     // Customize axis label font
@@ -59,31 +67,32 @@ MainWindow::MainWindow(QWidget *parent)
     axisY->setLinePen(axisPen);
 
     // Customize axis label colors
-    QBrush axisBrush(Qt::white);
+    QBrush axisBrush(Qt::black);
     axisX->setLabelsBrush(axisBrush);
     axisY->setLabelsBrush(axisBrush);
 
     // Customize grid lines and shades
-    axisX->setGridLineVisible(false);
+    axisX->setGridLineVisible(true);
     axisY->setGridLineVisible(false);
     axisY->setShadesPen(Qt::NoPen);
     axisY->setShadesBrush(QBrush(QColor(0x99, 0xcc, 0xcc, 0x55)));
-    axisY->setShadesVisible(true);
+    axisY->setShadesVisible(false);
 
-    axisX->append("low", 10);
-    axisX->append("optimal", 20);
-    axisX->append("high", 30);
-    axisX->setRange(0, 30);
+    axisX->setRange(0, 120);
+    axisX->setLabelFormat("%d");
+    axisY->append("T1", 1.5);
+    axisY->setRange(0, 3);
 
-    axisY->append("slow", 10);
-    axisY->append("med", 20);
-    axisY->append("fast", 30);
-    axisY->setRange(0, 30);
+    axisX->setTickCount(25);
+    axisY->setTickCount(3);
 
     chart->addAxis(axisX, Qt::AlignBottom);
     chart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisX);
     series->attachAxis(axisY);
+
+    seriesTask->attachAxis(axisX);
+    seriesTask->attachAxis(axisY);
 
     QChartView *chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
